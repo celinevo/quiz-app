1b8dff91475615ac49dee059a9432038abf60803
Celine Vogel <vogelceline@gmx.de>
2022-01-27 11:33:43 +0100

    tried something

--- basic.js
+++ basic.js
@@ -1,8 +1,10 @@
 const bookmarkElement = document.querySelector('[data-js="bookmark-clicked"]');
 
-bookmarkElement.addEventListener('click', () => {
+bookmarkElement?.addEventListener('click', () => {
   bookmarkElement.classList.toggle('card__bookmark--clicked');
 });
+
+// New Dings
 
 const outputElement = document.querySelector('[data-js="output"]');
 const surveyTextareaElement = document.querySelector(
@@ -10,7 +12,18 @@ const surveyTextareaElement = document.querySelector(
 );
 const counter = 200;
 
-document.body.addEventListener((input) => {
+/*surveyTextareaElement.addEventListener((input) => {
   const stringLength = surveyTextareaElement.value.length;
+  console.log(stringLength);
   outputElement.value = 200 - stringLength;
+});*/
+
+surveyTextareaElement?.addEventListener('keydown', (event) => {
+  console.log('hallo');
+  if (event.key === 'Backspace') {
+    counter = counter - 1;
+  } else {
+    counter = counter + 1;
+  }
+  outputElement.value = counter;
 });
