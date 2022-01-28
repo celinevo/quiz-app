64836ec4e2dcbfa4bfb7604a366828cedafab758
Celine Vogel <vogelceline@gmx.de>
2022-01-28 19:44:16 +0100

    Added js to profil to remove profil-user when loged out

--- js/logoutButton.js
+++ js/logoutButton.js
@@ -1,5 +1,7 @@
 export function logoutButton() {
   const logoutButton = document.querySelector('[data-js="user-logout"]');
+  const userProfil = document.querySelector('[data-js="user-profil"]');
+  const userLogoutText = document.querySelector('[data-js="user-logout-text"]');
 
   const textLogin = 'Login';
   const textLogout = 'Logout';
@@ -7,8 +9,12 @@ export function logoutButton() {
   logoutButton?.addEventListener('click', () => {
     if (logoutButton.textContent === 'Login') {
       logoutButton.textContent = 'Logout';
+      userProfil.classList.remove('user-profil--hidden');
+      userLogoutText.classList.add('user-logout-text--hidden');
     } else {
       logoutButton.textContent = 'Login';
+      userProfil.classList.add('user-profil--hidden');
+      userLogoutText.classList.remove('user-logout-text--hidden');
     }
   });
 }
